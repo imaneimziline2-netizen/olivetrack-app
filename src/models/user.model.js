ad7c3b64e474f38cf6587381257c78bd9314695b
imaneimziline2-netizen <imaneimziline2@gmail.com>
2026-08-31 00:12:24 +0100

    eat(models): add User model with role and statut fields

---
+++ src/models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    nom: { type: String, required: true },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    motDePasse: { type: String, required: true, select: false },
+    role: {
+        type: String,
+        enum: ["agriculteur", "admin"],
+        default: "agriculteur",
+    },
+    statut: { type: String, enum: ["actif", "desactive"], default: "actif" },
+},
+{timestamps: true});
+
+export default mongoose.model("User", userSchema);
