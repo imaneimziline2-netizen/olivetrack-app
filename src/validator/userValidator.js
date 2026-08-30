18214af7ac994448ccc171d82a84aa384f895b92
imaneimziline2-netizen <imaneimziline2@gmail.com>
2026-08-31 00:12:43 +0100

    feat(validator): add Joi validators for register and login

---
+++ src/validator/userValidator.js
@@ -0,0 +1,12 @@
+import Joi from "joi";
+
+export const registerValidator = Joi.object({
+    nom : Joi.string().min(2).required(),
+    email: Joi.string().email().required(),
+    motDePasse: Joi.string().min(8).required(), 
+})
+
+export const loginValidator = Joi.object({
+    email: Joi.string().email().required(),
+    motDePasse: Joi.string().required(),
+})
