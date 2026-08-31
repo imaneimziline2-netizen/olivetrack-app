75dc2d08a3610cc60fadd974cc70ec55034c8a3f
imaneimziline2-netizen <imaneimziline2@gmail.com>
2026-08-31 13:34:30 +0100

    feat(validator): add updateProfileValidator for profile updates

--- src/validator/userValidator.js
+++ src/validator/userValidator.js
@@ -10,3 +10,9 @@ export const loginValidator = Joi.object({
     email: Joi.string().email().required(),
     motDePasse: Joi.string().required(),
 })
+
+
+export const updateProfileValidator = Joi.object({
+    nom: Joi.string().min(2),
+    email: Joi.string().email(),
+})
