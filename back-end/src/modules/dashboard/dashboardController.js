c652a71a6ff87d98b71ade11efdeca0b1a37136d
imaneimziline2-netizen <imaneimziline2@gmail.com>
2026-09-14 18:09:50 +0100

    feat(dashboard): add getMonthlyYield controller endpoint

--- back-end/src/modules/dashboard/dashboardController.js
+++ back-end/src/modules/dashboard/dashboardController.js
@@ -2,6 +2,7 @@ import { serverErrorResponse } from "../../utils/serverErrorResponse.js";
 import {
     rendementToutesParcelles,
     comparerRendementParcelle,
+    rendementMensuelGlobal,
 } from "./statsService.js";
 
 export async function getDashboard(req, res) {
@@ -26,3 +27,15 @@ export async function getRendementParcelle(req, res) {
         serverErrorResponse(res, err);
     }
 }
+
+// GET /api/dashboard/monthly?annee=2026
+export async function getMonthlyYield(req, res) {
+    try {
+        const annee = parseInt(req.query.annee) || new Date().getFullYear();
+        const resultat = await rendementMensuelGlobal(req.user.userId, annee);
+        res.json(resultat);
+    } catch (err) {
+        serverErrorResponse(res, err);
+    }
+}
+
